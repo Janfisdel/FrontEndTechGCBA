14b14364edabdea87599c8eb5a5e88953c10bad4
Jana Fisdel <janafisdel@gmail.com>
2025-07-05 21:28:59 -0300

    modificacion de estilos y reseñas

--- JS/productos.js
+++ JS/productos.js
@@ -1,60 +1,60 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+document.addEventListener("DOMContentLoaded", () => {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const renderizarProductos = ()=>{
-fetch(('https://dummyjson.com/products?limit=10'))
-.then(response =>response.json())
+  const renderizarProductos = () => {
+    fetch("https://dummyjson.com/products?limit=10")
+      .then((response) => response.json())
 
-.then(data=>{ 
-    let contenedorProductos = document.getElementById("contenedor-productos")
-    for(const producto of data.products){
-        let tarjetaProducto = document.createElement("article")
-        tarjetaProducto.classList.add("tarjeta-producto")
-       let tituloProducto = document.createElement("h3")
-       tituloProducto.classList.add("titulo-producto")
-       tituloProducto.textContent = producto.title
+      .then((data) => {
+        let contenedorProductos = document.getElementById(
+          "contenedorProductos"
+        );
+        for (const producto of data.products) {
+          let tarjetaProducto = document.createElement("article");
+          tarjetaProducto.classList.add("tarjetaProducto");
+          let tituloProducto = document.createElement("h3");
+          tituloProducto.classList.add("tituloProducto");
+          tituloProducto.textContent = producto.title;
 
-       let precioProducto = document.createElement("p")
-       precioProducto.textContent=`$${producto.price}`
-    
-       let imagenProducto = document.createElement("img")
-       imagenProducto.src = producto.images[0]
-       imagenProducto.classList.add("img-producto")
-       imagenProducto.alt = producto.description;
+          let precioProducto = document.createElement("p");
+          precioProducto.textContent = `$${producto.price}`;
 
-       let botonAgregar = document.createElement("button")
-       botonAgregar.textContent ="Agregar"
-       botonAgregar.classList.add("btn-agregar")
-       botonAgregar.addEventListener("click", ()=>{
-        alert(`Se agrego ${producto.title} al carrito`)
+          let imagenProducto = document.createElement("img");
+          imagenProducto.src = producto.images[0];
+          imagenProducto.classList.add("imgProducto");
+          imagenProducto.alt = producto.description;
 
-        agregarProductos(producto)
-        actualizarAgregados()
+          let botonAgregar = document.createElement("button");
+          botonAgregar.textContent = "Agregar";
+          botonAgregar.classList.add("btnAgregar");
+          botonAgregar.addEventListener("click", () => {
+            alert(`Se agrego ${producto.title} al carrito`);
 
-       })
-       
-       tarjetaProducto.appendChild(tituloProducto)
-       tarjetaProducto.appendChild(precioProducto)
-       tarjetaProducto.appendChild(imagenProducto)
-       tarjetaProducto.appendChild(botonAgregar)
-       contenedorProductos.appendChild(tarjetaProducto)
-    }
-    })
-     .catch(error=>{
-    console.error('Error al obtener productos:', error)
-})
+            agregarProductos(producto);
+            actualizarAgregados();
+          });
 
-}
+          tarjetaProducto.appendChild(tituloProducto);
+          tarjetaProducto.appendChild(precioProducto);
+          tarjetaProducto.appendChild(imagenProducto);
+          tarjetaProducto.appendChild(botonAgregar);
+          contenedorProductos.appendChild(tarjetaProducto);
+        }
+      })
+      .catch((error) => {
+        console.error("Error al obtener productos:", error);
+      });
+  };
 
-const agregarProductos = producto =>{
-    carrito.push(producto)
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-}
+  const agregarProductos = (producto) => {
+    carrito.push(producto);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  };
 
-const actualizarAgregados = ()=>{
-    const contadorCarrito= document.getElementById("contador-carrito")
-    contadorCarrito.textContent = carrito.length
-}
-renderizarProductos()
-actualizarAgregados()
-})
+  const actualizarAgregados = () => {
+    const contadorCarrito = document.getElementById("contadorCarrito");
+    contadorCarrito.textContent = carrito.length;
+  };
+  renderizarProductos();
+  actualizarAgregados();
+});
